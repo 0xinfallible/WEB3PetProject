27f41992ded2fe5d5bdd5fd7f96091133146bd60
ShayaanAkbar <jbarbarbar@protonmail.com>
2022-06-10 13:06:32 -0400

    Metamask installed

--- nft-collectible-frontend/src/App.js
+++ nft-collectible-frontend/src/App.js
@@ -1,15 +1,53 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import contract from './contracts/NFTCollectible.json';
+import { ethers } from 'ethers';
 
 const contractAddress = "0x355638a4eCcb777794257f22f50c289d4189F245";
 const abi = contract.abi;
 
+
 function App() {
 
-  const checkWalletIsConnected = () => { }
+  const [currentAccount, setCurrentAccount] = useState(null);
 
-  const connectWalletHandler = () => { }
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet exists! We're ready to go!")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
 
   const mintNftHandler = () => { }
 
